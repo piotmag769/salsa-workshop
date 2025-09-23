ee44b30c62b2d9125c70b0237fd1126d3ec14887
piotmag769 <piotrmagiera150@gmail.com>
2025-09-23 17:26:12 +0200

    Task 2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,8 @@ mod lexer;
 mod parser;
 mod solver;
 
+use salsa::Setter;
+
 use crate::db::SpreadsheetDatabase;
 use crate::input::raw::RawSpreadsheet;
 use crate::parser::ParserGroup;
@@ -17,13 +19,46 @@ macro_rules! row {
 }
 
 fn main() {
-    let db: SpreadsheetDatabase = Default::default();
-    let cells = vec![row! {"5" | "$0:0 + $1:0"}, row! {"7" | "$0:1 + $1:0"}];
+    let mut db: SpreadsheetDatabase = Default::default();
 
-    let raw_spreadsheet = RawSpreadsheet::new(&db, cells);
+    // This symbolises changes in the state of a spreadsheet over time.
+    let mut queue = [
+        [
+            row! {"3" | "5"  | "$0:0 + $0:1"},
+            // row! {"7" | "12" | "$1:0 + $1:1"},
+            // row! {"1" | "13" | "$2:0 + $2:1"},
+        ],
+        // User changed cell 0:1.
+        [
+            row! {"3" | "5 - 3"  | "$0:0 + $0:1"},
+            // row! {"7" | "12" | "$1:0 + $1:1"},
+            // row! {"1" | "13" | "$2:0 + $2:1"},
+        ],
+        // // User changed cell 2:2.
+        // [
+        //     row! {"3" | "5 - 3"  | "$0:0 + $0:1"},
+        //     row! {"7" | "12" | "$1:0 + $1:1"},
+        //     row! {"1" | "13" | "$2:0 + $2:1 - 5"},
+        // ],
+        // // User changed cell 1:1.
+        // [
+        //     row! {"3" | "5 - 3"  | "$0:0 + $0:1"},
+        //     row! {"7" | "$0:0 + $2:2" | "$1:0 + $1:1"},
+        //     row! {"1" | "13" | "$2:0 + $2:1 - 5"},
+        // ],
+    ]
+    .to_vec();
 
-    // Run queries.
-    let parsed_spreadsheet = db.parse_spreadsheet(raw_spreadsheet);
-    let solved_spreadsheet = db.solve_spreadsheet(parsed_spreadsheet);
-    eprintln!("{solved_spreadsheet:?}");
+    let raw_spreadsheet = RawSpreadsheet::new(&db, Default::default());
+
+    queue.reverse();
+    while let Some(cells) = queue.pop() {
+        // Set new input.
+        todo!("set `cells` in `raw_spreadsheet` to `cells`");
+
+        // Run queries.
+        let parsed_spreadsheet = db.parse_spreadsheet(raw_spreadsheet);
+        let solved_spreadsheet = db.solve_spreadsheet(parsed_spreadsheet);
+        eprintln!("{solved_spreadsheet:?}");
+    }
 }
