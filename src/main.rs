d20983b34a44154db0b00317336e74488499d97f
piotmag769 <piotrmagiera150@gmail.com>
2025-09-24 12:55:00 +0200

    Task 5

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,15 @@ mod ir;
 mod lexer;
 mod parser;
 mod solver;
-
-use salsa::Setter;
+mod threads;
 
 use crate::db::SpreadsheetDatabase;
 use crate::input::raw::RawSpreadsheet;
 use crate::parser::ParserGroup;
 use crate::solver::SolverGroup;
+use crate::threads::parse_cell_on_another_thread;
+use salsa::Setter;
+use std::any::Any;
 
 macro_rules! row {
     ($($cell_str:literal) | *) => {
@@ -25,29 +27,19 @@ fn main() {
     // This symbolises changes in the state of a spreadsheet over time.
     let mut queue = [
         [
-            row! {"13 14" | "5 + "     | "$0:0 + $0:1"},
-            // row! {"7 + -" | "12 + bro" | "$1:0 + $1:1"},
-            // row! {"+ 1"   | "13"       | "$2:0 + $2:1"},
+            row! {"$0:2" | "5 - 3"  | "$0:1 + $0:0"},
+            row! {"7" | "12" | "$1:0 + $1:1"},
+            row! {"1" | "13" | "$2:0 + $2:1"},
         ],
         [
-            row! {"$0:2" | "5 - 3"  | "$0:1 + $0:0"},
-            // row! {"7" | "12" | "$1:0 + $1:1"},
-            // row! {"1" | "13" | "$2:0 + $2:1"},
+            row! {"3" | "5 - 3"  | "$0:0 + $0:1"},
+            row! {"7" | "12" | "$1:0 + $1:1"},
+            row! {"420" | "13" | "$2:0 + $2:1 - 5"},
         ],
-        // // User changed cell 2:2.
-        // [
-        //     row! {"3" | "5 - 3"  | "$0:0 + $0:1"},
-        //     row! {"7" | "12" | "$1:0 + $1:1"},
-        //     row! {"1" | "13" | "$2:0 + $2:1 - 5"},
-        // ],
-        // // User changed cell 1:1.
-        // [
-        //     row! {"3" | "5 - 3"  | "$0:0 + $0:1"},
-        //     row! {"7" | "$0:0 + $2:2" | "$1:0 + $1:1"},
-        //     row! {"1" | "13" | "$2:0 + $2:1 - 5"},
-        // ],
     ]
     .to_vec();
+    let rows = queue[0].len();
+    let cols = queue[0][0].len();
 
     let raw_spreadsheet = RawSpreadsheet::new(&db, Default::default());
 
@@ -56,11 +48,26 @@ fn main() {
         // Set new input.
         raw_spreadsheet.set_cells(&mut db).to(cells.to_vec());
 
-        // Run queries.
-        let (parsed_spreadsheet, diags) = db.parse_spreadsheet(raw_spreadsheet);
-        eprintln!("Parser diags: {diags:#?}");
-        let (solved_spreadsheet, diags) = db.solve_spreadsheet(parsed_spreadsheet);
-        eprintln!("Solver diags: {diags:#?}");
-        eprintln!("{solved_spreadsheet:?}");
+        let mut handles = Vec::new();
+        for row in 0..rows {
+            for col in 0..cols {
+                if row == 1 && col == 1 {
+                    // TODO: uncomment after completing the task and see what happens!
+                    // raw_spreadsheet
+                    //     .set_cells(&mut db)
+                    //     .to(queue.pop().unwrap().to_vec());
+                }
+                // Clone to send to another thread.
+                // SAFETY: do NOT mutate multiple databases or else the program may deadlock.
+                let db_clone = db.clone();
+                let handle = parse_cell_on_another_thread(db_clone, raw_spreadsheet, row, col);
+
+                handles.push(handle);
+            }
+        }
+
+        for handle in handles {
+            eprintln!("{}", handle.join().unwrap());
+        }
     }
 }
