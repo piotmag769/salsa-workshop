65f2918bfbd1b9063f151b78f883239c289d2dcd
piotmag769 <piotrmagiera150@gmail.com>
2025-09-24 18:24:19 +0200

    Task 7

--- src/db.rs
+++ src/db.rs
@@ -1,50 +1,35 @@
 use salsa::EventKind;
-use std::sync::{Arc, Mutex};
 
 #[salsa::db]
 #[derive(Clone)]
 pub struct SpreadsheetDatabase {
     storage: salsa::Storage<Self>,
-    logs: Arc<Mutex<Vec<String>>>,
 }
 
 impl salsa::Database for SpreadsheetDatabase {}
 
 impl Default for SpreadsheetDatabase {
     fn default() -> Self {
-        let logs = <Arc<Mutex<Vec<String>>>>::default();
         Self {
             storage: salsa::Storage::new(Some(Box::new({
-                let logs = logs.clone();
                 move |event| match event.kind {
                     EventKind::DidValidateMemoizedValue { .. }
                     | EventKind::WillBlockOn { .. }
                     | EventKind::WillExecute { .. }
                     | EventKind::WillIterateCycle { .. }
                     | EventKind::DidDiscard { .. }
-                    | EventKind::DidSetCancellationFlag
-                    | EventKind::WillCheckCancellation => {
+                    | EventKind::DidSetCancellationFlag => {
                         eprintln!("{:?}", event.kind);
-                        let mut logs = logs.lock().unwrap();
-                        logs.push(format!("{:?}", event.kind))
                     }
 
                     EventKind::DidDiscardAccumulated { .. }
+                    | EventKind::WillCheckCancellation
                     | EventKind::DidInternValue { .. }
                     | EventKind::DidReuseInternedValue { .. }
-                    | EventKind::DidValidateInternedValue { .. } => {}
-
-                    EventKind::WillDiscardStaleOutput { .. } => {}
+                    | EventKind::DidValidateInternedValue { .. }
+                    | EventKind::WillDiscardStaleOutput { .. } => {}
                 }
             }))),
-            logs,
         }
-    }
-}
-
-impl Drop for SpreadsheetDatabase {
-    fn drop(&mut self) {
-        let _logs = self.logs.lock().unwrap();
-        // eprintln!("{:#?}", _logs)
     }
 }
